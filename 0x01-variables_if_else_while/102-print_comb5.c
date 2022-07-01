9a249878f6e0726ac87b0f36fa0f30f858ce3106
osala-eng <jashonosala@gmail.com>
2022-07-01 15:20:38 -0700

    Test 8

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,38 +10,20 @@
 int main(void)
 {
 	int num, i, j, k;
-	int done = false;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 98; num++)
 	{
-		if (done)
-			break;
-		for (i = 0; i < 10; i++)
+		for (i = num + 1; i <= 99; i++)
 		{
-			if (done)
-				break;
-			for (j = num; j < 10; j++)
-			{
-				if (done)
-					break;
-				for (j > 0 ? (k = 0) : (k = i + 1); k < 10; k++)
-				{
-					if (num == j && k == i)
-						continue;
-					putchar(num + '0');
-					putchar(i + '0');
-					putchar(' ');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (num == 9 && i == 8 && j == 9 && k == 9)
-					{
-						done = true;
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			if (num == 98 && i == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
