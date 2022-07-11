f2e4448fe215670c4303b5c1c7bb5b4b11d88ff7
Jashon Osala <jashonosala@gmail.com>
2022-07-11 07:34:22 +0300

    Task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,15 @@
 #include <string.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
  * reset_to_98 - reset the value of a pointer to 98
  * @n: integer pointer
  *
@@ -27,5 +36,14 @@ void swap_int(int *a, int *b);
  * Return: integer len
  */
 int _strlen(char *s);
+
+/**
+ * _puts - print to the stdout
+ * @str: string
+ *
+ * Return: void
+ */
+
+void _puts(char *str);
 
 #endif
