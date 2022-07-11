548e9312ff60ca2d24fb5e6cb6a6bd2018441970
Jashon Osala <jashonosala@gmail.com>
2022-07-11 06:53:04 +0300

    Task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * reset_to_98 - reset the value of a pointer to 98
+ * @n: integer pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n);
+
+
+
+#endif
