2139faa36dda7f04a75d8e9b013b2aa02c1f3673
Jashon Osala <jashonosala@gmail.com>
2022-07-13 22:36:28 +0300

    Task 0 done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,6 +2,8 @@
 
 /**
  * _strcat - concatenate two strings
+ * @dest: char pointer
+ * @src: char pointer
  *
  * Return: char *
  */
@@ -9,14 +11,16 @@
 char *_strcat(char *dest, char *src)
 {
 	int len, _len, i;
+	char c;
 
 	i = 0;
 	len = strlen(src);
 	_len = strlen(dest);
-	
-	for (i = 0; i < len; i++)
+
+	for (i = 0; i <= len; i++)
 	{
-		dest[_len + i] = src[i];
+		c = i < len ? src[i] : '\0';
+		dest[_len + i] = c;
 	}
 	return (dest);
 }
