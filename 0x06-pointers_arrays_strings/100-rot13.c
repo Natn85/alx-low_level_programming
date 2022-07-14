2e3a8ffc962794a1c47e6302ff4e108691fe9369
Jashon Osala <jashonosala@gmail.com>
2022-07-14 03:58:23 +0300

    Betty fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -24,7 +24,8 @@ char *rot13(char *s)
 
 	for (j = 0; s[j] != '\0'; j++)
 	{
-		for (i = 0; a[i] != '\0' && s[j] != a[i]; i++);
+		for (i = 0; a[i] != '\0' && s[j] != a[i]; i++)
+			;
 
 		if (s[j] == a[i])
 			s[j] = b[i];
