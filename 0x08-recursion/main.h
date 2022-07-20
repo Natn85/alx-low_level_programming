4efcc5b6bd2f27becffb58ed82edd8e59d7ab23b
Jashon Osala <jashonosala@gmail.com>
2022-07-20 14:19:05 +0300

    Task 8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -83,4 +83,13 @@ int is_prime_number(int n);
 
 int is_palindrome(char *s);
 
+/**
+ * wildcmp - compare if string with wildcard mattches
+ * @s1: string 1
+ * @s2: string 2
+ * Return: SUCCESS ? 1 : 0
+ */
+
+int wildcmp(char *s1, char *s2);
+
 #endif
