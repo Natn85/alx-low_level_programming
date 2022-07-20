1dabf44f129d72a9640f48071bd166cd153458c3
Jashon Osala <jashonosala@gmail.com>
2022-07-20 08:01:23 +0300

    Created main.h file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - print a string using recursion
+ * @s: String to print
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s);
+
+#endif
