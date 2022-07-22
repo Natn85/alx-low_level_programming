026735ab0e142906daa038a3faf79763695c1b97
Jashon Osala <jashonosala@gmail.com>
2022-07-22 07:30:43 +0300

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of params
+ * @argv: array of params
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
