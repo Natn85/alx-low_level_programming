27416949df79cbc4aada7930eac648abb17df608
Jashon Osala <jashonosala@gmail.com>
2022-07-28 10:58:13 +0300

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * string_nconcat - allocates memory using malloc
+ * @s1: string
+ * @s2: string
+ * @n: length of string
+ *
+ * Return: pointer to memory location
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	int i, k;
+
+	if (!s1)
+		s1 = EMPTY;
+	if (!s2)
+		s2 = EMPTY;
+
+	s = malloc(sizeof(s1) + (sizeof(char) * n));
+	if (!s)
+		return (NULL);
+
+	i = 0;
+	while (s1[i] != END)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	k = 0;
+	while (s2[k] != END && k < (int) n)
+		s[i++] = s2[k++];
+
+	s[i++] = END;
+
+	return (s);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,15 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#define END '\0'
+
+#define EMPTY ""
+
 #include <stdio.h>
 
 #include <stdlib.h>
+
+#include <string.h>
 
 /**
  * malloc_checked - allocates memory using malloc
@@ -13,5 +19,16 @@
  */
 
 void *malloc_checked(unsigned int b);
+
+/**
+ * string_nconcat - allocates memory using malloc
+ * @s1: string
+ * @s2: string
+ * @n: length of string
+ *
+ * Return: pointer to memory location
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
