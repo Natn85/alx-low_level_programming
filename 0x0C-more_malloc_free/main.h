535bcbe939c56518ce37dadf5a39a2a58413f050
Jashon Osala <jashonosala@gmail.com>
2022-07-28 10:28:42 +0300

    Task 1

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to allocate
+ *
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b);
+
+#endif
