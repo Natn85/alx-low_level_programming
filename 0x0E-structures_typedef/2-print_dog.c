378a466bf5ee01879e0e04934663e0a94b2b0a93
Jashon Osala <jashonosala@gmail.com>
2022-08-01 14:33:33 +0300

    Cleaned for betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-	
+
 	printf("Name: %s\n", (*d).name ? (*d).name : NIL);
 	printf("Age: %f\n", (*d).age);
 	printf("Owner: %s\n", (*d).owner ? (*d).owner : NIL);
