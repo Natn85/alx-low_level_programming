6c3f1db536a8961d0e54204d31723c9758d06231
Jashon Osala <jashonosala@gmail.com>
2022-08-03 20:35:36 +0300

    Conf main.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -18,8 +18,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -27,13 +27,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
-/**
- * get_op_func - select the correct function
- * @s: input arguments
- *
- * Return: func pointer
- */
 
 int (*get_op_func(char *s))(int, int);
 
