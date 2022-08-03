835a53121c372f9a39f2cf3efbd0e428f029f8fc
Jashon Osala <jashonosala@gmail.com>
2022-08-03 16:57:46 +0300

    Function pointer array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _FUNCTION_POINTER_H
 #define _FUNCTION_POINTER_H
 
+#include <stddef.h>
+
 /**
  * print_name - prints a name
  * @name: string
@@ -10,5 +12,16 @@
  */
 
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * array_iterator - execute a func on each array
+ * @array: array
+ * @size: size of array
+ * @action: pointer to a func
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
