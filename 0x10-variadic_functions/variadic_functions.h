eb9c4cdfb8d58a157bd30431d56997c94ce5fd20
Jashon Osala <jashonosala@gmail.com>
2022-08-04 02:08:21 +0300

    Task 2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,12 @@
 #ifndef _VARIADIC_FUNCS_H
 #define _VARIADIC_FUNCS_H
 
+#define NIL "(nil)"
+
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * sum_them_all - sum all params
@@ -22,5 +26,15 @@ int sum_them_all(const unsigned int n, ...);
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...);
+
+/**
+ * print_strings - print strings
+ * @separator: separator
+ * @n: num of params
+ *
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
