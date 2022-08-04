387d0f1e7a5c0128e64241fbdc5c5c57fe72ea3d
Jashon Osala <jashonosala@gmail.com>
2022-08-04 12:25:25 +0300

    Fix 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,10 +18,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		buffer = strdup(va_arg(params, char *));
-
+		buffer = va_arg(params, char *);
 		printf("%s", buffer ? buffer : NIL);
-		free(buffer);
 		if (!separator || i == n - 1)
 			continue;
 
