ffc45231d88641d79c3ff47ba2e8be5f3951559c
Jashon Osala <jashonosala@gmail.com>
2022-08-15 20:51:27 +0300

    Fix null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *mem_loc;
 
-	if (!*head)
+	if (!(*head))
 		return;
 
 	for (; *head;)
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - get node at given index
+ * @head: list
+ * @idx: index
+ * @n: data to insert
+ *
+ * Return: new node address or NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	if (!head)
+		return (NULL);
+
+	for (; head && index; index--)
+		head = head->next;
+
+	if (!index)
+		return (head);
+
+	return (NULL);
+}
