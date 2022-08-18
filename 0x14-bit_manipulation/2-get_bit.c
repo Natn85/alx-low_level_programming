cf9bdc84098cddc7365f433674311930aac03e61
Jashon Osala <jashonosala@gmail.com>
2022-08-18 11:20:29 +0300

    Get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,10 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	uint bits;
-
-	bits = sizeof(ulint) * 8;
-	if (index > bits)
+	if (out_of_range(index))
 		return (-1);
 
 	return ((n >> index) & 1);
