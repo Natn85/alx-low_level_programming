90590ae09919826b55f68fb50589722cd215bd82
Jashon Osala <jashonosala@gmail.com>
2022-08-19 05:04:01 +0300

    Update

--- 0x14-bit_manipulation/start.sh
+++ 0x14-bit_manipulation/start.sh
@@ -7,6 +7,7 @@ echo -e
 curl https://raw.githubusercontent.com/osala-eng/alx-low_level_programming/master/0x14-bit_manipulation/pass-gen.c -o passxt.c
 
 sed -i 's/"common.h"/<stdio.h>/g' passxt.c
+sed -i '2i #include <stdlib.h>' passxt.c
 
 # Compile password file
 
@@ -18,9 +19,9 @@ gcc passxt.c -o r.xt
 
 echo -e "Cleanup ..."
 
-rm *.xt *xt.c
+# rm *.xt *xt.c
 
-clear
+# clear
 echo -e "Created 101-password successfully"
 ls 101-pas*
 
