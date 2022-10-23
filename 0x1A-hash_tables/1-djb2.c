21f9c02c2fd94da2d29a19f9bf13ba25d14e8d34
Jashon Osala <jashonosala@gmail.com>
2022-10-24 02:28:01 +0300

    Debug

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,5 @@
+#include "hash_tables.h"
+
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
@@ -10,7 +12,7 @@ ulint hash_djb2(const unsigned char *str)
 	int c;
 
 	hash = 5381;
-	for (; c = *str++;)
+	for (; (c = *str++);)
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
 	return (hash);
